ac10ca9625764820c9faba92f7a3dd5eeaddd7db
emigreif <157245016+emigreif@users.noreply.github.com>
2025-03-20 11:37:43 -0300

    wew

---
+++ models/PerfilGeneral.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const perfilGeneralSchema = new mongoose.Schema({
+  extrusora: {
+    type: String,
+    enum: ["Aluar", "Hydro", "Alcemar", "Aluwind", "Flamia MDT", "Rehau", "Munchtek", "Aluplast", "Veratek", "Veka", "Otro"],
+    required: true
+  },
+  linea: { type: String, required: true },
+  codigo: { type: String, required: true, unique: true },
+  descripcion: { type: String, required: true },
+  largo: { type: Number, required: true }, // Largo en mm
+  pesoxmetro: { type: Number, required: true } // Peso en kg/m
+});
+
+const PerfilGeneral = mongoose.model("PerfilGeneral", perfilGeneralSchema);
+export default PerfilGeneral;
---
+++ models/VidrioGeneral.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const vidrioGeneralSchema = new mongoose.Schema({
+  descripcion: { type: String, required: true },
+  espesor: { type: Number, required: true },
+  peso_m2: { 
+    type: Number, 
+    required: true,
+    default: function() { return this.espesor * 2.5; } // Cálculo automático
+  }
+});
+
+const VidrioGeneral = mongoose.model("VidrioGeneral", vidrioGeneralSchema);
+export default VidrioGeneral;
